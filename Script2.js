c4dfee44ae6ad01052a25f99da5bc0acfd1987be
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-06-12 13:36:51 -0400

    Add files via upload

--- Script2.js
+++ Script2.js
@@ -73,7 +73,7 @@ Main.style.background = 'black';
   }
 });
 
-  var CANALES = ['.m3u8', 'bit.ly', 'stream', 'is.gd', 'lamovie'];  
+  var CANALES = ['.m3u8', 'bit.ly', 'stream', 'is.gd', 'lamovie.link'];  
 
          <!-- HTML -->
          
